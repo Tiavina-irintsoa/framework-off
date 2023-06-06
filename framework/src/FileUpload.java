39ca51e05fdcac3babc591afbd112bf302c39b1e
Tiavina-irintsoa <rakotonirinairintsoa0@gmail.com>
2023-06-06 10:28:22 +0300

    Sprint 10: upload de fichiers

---
+++ framework/src/FileUpload.java
@@ -0,0 +1,30 @@
+package etu1840.framework.util;
+public class FileUpload {
+    private String name;
+    private String path;
+    private byte[] bytes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public byte[] getBytes() {
+        return bytes;
+    }
+
+    public void setBytes(byte[] bytes) {
+        this.bytes = bytes;
+    }
+}
